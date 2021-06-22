19693eba8a67a649060ab72ce56c28ad672db489
Kanishka Hewapathirana <KanishkaH@embla.asia>
2021-06-22 14:16:26 +0530

    Add types on behalf of classes

--- src/Models/TaskItem.ts
+++ src/Models/TaskItem.ts
@@ -1,18 +1,5 @@
-// export default class TaskItem{
-//     id: number;
-//     title: string;
-//     completed: boolean;
-
-//     constructor() {            
-//        this.title = "";
-//        this.completed = false; 
-//     }
-// }
-
 export  type TaskItem = {
     id?: number;
     title: string;
-    completed: boolean;
-
-   
+    completed: boolean;   
 }
